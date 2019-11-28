d954c29f1ed93ca74d8fed5dc451cceff0d07494
Refayat Haque <refayathaque@gmail.com>
2019-11-28 17:30:25 -0500

    1730 Nov 28 2019

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-## Refayat Haque - API Interview Question V2 - Senior Backend Engineer - Toronto, Ontario, Canada
-
-## Assumptions:
-- Single item storage flat fee: $20
-- Large item storage extra charge: $1/sq foot
-- High value goods extra charge: 5% of value of good(s)
-- Increasing discounts per 100 items stored (only goes up to 200 items)
-  - By increments of 5% (e.g. 100 items: 5%, 200 items: 10%, 210 items: 15%, 267 items: 15%, 688 items: 15%)
-- Sales team should be able to provide discounts at their own discretion (not exceeding 20%)
-- When monthly bill reaches $400 monthly, automatically issue a $200 discount to the yearly bill
+## API Interview Question V2 - Refayat Haque - "Senior Backend Engineer" candidate - Toronto, Ontario, Canada
--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React from 'react';
 import './mystyles.scss';
 
-function App() {
-  return (
-    <div>
-      <section class="hero is-warning">
-        <div class="hero-body">
-          <div class="container">
-            <h1 class="title">
+const App = () => {
+
+  const renderHeader = () => {
+    return (
+      <section className="hero is-warning">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
               API Interview Question V2 <span><a href="" target="_blank" className="is-inline icon">
                 <i className="fab fa-lg fa-github"></i>
               </a></span>
             </h1>
-            <h2 class="subtitle">
+            <h2 className="subtitle">
               Refayat Haque <span><a href="" target="_blank" className="is-inline icon">
                 <i className="fab fa-lg fa-linkedin"></i>
-              </a></span> - Applying for Senior Backend Engineer
+              </a></span> - "Senior Backend Engineer" candidate
             </h2>
           </div>
         </div>
       </section>
+    )
+  }
+
+  const renderInput = () => {
+    return (
+      <div>
+        Input
+      </div>
+    )
+  }
+
+  const renderOutput = () => {
+    return (
+      <div>
+        Output
+      </div>
+    )
+  }
+
+  const renderBody = () => {
+    return (
+      <div className="container">
+        <div class="columns">
+          <div class="column">
+            {renderInput()}
+          </div>
+          <div class="column">
+            {renderOutput()}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {renderHeader()}
+      {renderBody()}
     </div>
   );
 }
