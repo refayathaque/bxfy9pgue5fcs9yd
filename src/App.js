3336a310105c25fc5f3f56e7d2e5188f6be518c7
Refayat Haque <refayathaque@gmail.com>
2019-11-28 12:58:27 -0500

    1258 Nov 28 2019

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './mystyles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section class="hero is-warning">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              API Interview Question V2 <span><a href="" target="_blank" className="is-inline icon">
+                <i className="fab fa-lg fa-github"></i>
+              </a></span>
+            </h1>
+            <h2 class="subtitle">
+              Refayat Haque <span><a href="" target="_blank" className="is-inline icon">
+                <i className="fab fa-lg fa-linkedin"></i>
+              </a></span> - Applying for Senior Backend Engineer
+            </h2>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
